25493b5aed23474f98dd61dacdeb9bc1ba3f50a8
majidali129 <majidaliofficial129@gmail.com>
2026-08-13 16:07:55 +0500

    Update database configuration for Railway deployment

--- apps/api/src/database/database.provider.ts
+++ apps/api/src/database/database.provider.ts
@@ -7,15 +7,27 @@ export const databaseProviders = [
         provide: DATA_SOURCE,
         inject: [ConfigService],
         useFactory: async (configService: ConfigService) => {
+            // LOCALHOST SETUP
+            // const dataSource = new DataSource({
+            //     type: "postgres",
+            //     host: configService.get('POSTGRES_HOST'),
+            //     port: configService.get('POSTGRES_PORT'),
+            //     username: configService.get('POSTGRES_USER'),
+            //     password: configService.get('POSTGRES_PASSWORD'),
+            //     database: configService.get('POSTGRES_DATABASE'),
+            //     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            //     synchronize: true,
+            // });
+
+            // RAILWAY SETUP
             const dataSource = new DataSource({
                 type: "postgres",
-                host: configService.get('POSTGRES_HOST'),
-                port: configService.get('POSTGRES_PORT'),
-                username: configService.get('POSTGRES_USER'),
-                password: configService.get('POSTGRES_PASSWORD'),
-                database: configService.get('POSTGRES_DATABASE'),
+                url: configService.get('DATABASE_URL'),
                 entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-                synchronize: true,
+                synchronize: false,
+                ssl: {
+                    rejectUnauthorized: false,
+                }
             });
 
             return dataSource.initialize()
