1b879c4be34025ea66c62d6998048ba6f278a919
majidali129 <majidaliofficial129@gmail.com>
2026-08-18 11:01:59 +0500

    remoted unused import

--- apps/web/src/components/auth/hooks/use-signout.ts
+++ apps/web/src/components/auth/hooks/use-signout.ts
@@ -1,6 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useNavigate } from "react-router"
-import { toast } from "sonner"
 
 import type { ApiError } from "../../../api/client"
 import { authApi } from "../../../api/services/auth"
